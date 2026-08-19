2b3b223d8776904b0e115e6ed688a589fc7829c2
YanjieGu20 <jasongu2003@outlook.com>
2026-08-19 12:29:56 -0400

    v4

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 api_key = os.getenv("API_KEY")
 db_url = os.getenv("DATABASE_URL")
+
+print("this is for the test")
+print("This is a test print statement")
